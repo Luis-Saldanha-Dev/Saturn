6dba5705528804b7312637057d0022a78ce218b2
Fernando <luis.saldanha.dev@gmail.com>
2026-05-21 18:34:01 -0300

    Adição do componente heading

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,5 +1,13 @@
-import './styles.modules.css'
+import styles from './styles.module.css';
 
-export function Headder(){
-    return
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 }
