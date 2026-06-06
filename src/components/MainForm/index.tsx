355a9a7b2c7b60f627ea4346aaef3902a380a174
Luis Saldanha <168227026+LuisSaldanha-Dev@users.noreply.github.com>
2026-06-06 19:12:35 -0300

    Adicionando Page/Home e primeiro Template para o APP

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from "lucide-react";
+import { DefaultButton } from "../DefaltButton";
+import { Cycles } from "../Cycles";
+import { DefaultInput } from "../DefaultInput";
+
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='Task:'
+              id='meuInput'
+              type='text'
+              placeholder='Digite sua task'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+          </div>
+        </form>
+  )
+}
