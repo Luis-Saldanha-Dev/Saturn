95a5dbc0e4f8b5c6ef78ff68cc27011883028e56
Luis Saldanha <168227026+LuisSaldanha-Dev@users.noreply.github.com>
2026-06-06 18:14:05 -0300

    adicionando Funcionalidade no botao de alterar tema do menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,34 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon, MoonIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvaliableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvaliableThemes>(() => {
+    const storageTheme = (localStorage.getItem('theme') as AvaliableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'light' ? 'dark' : 'light';
+      return nextTheme;
+    });
+  }
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
       <a className={styles.menuLink} href='#'>
@@ -16,8 +43,8 @@ export function Menu() {
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
-        <SunIcon />
+      <a className={styles.menuLink} href='#' onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
