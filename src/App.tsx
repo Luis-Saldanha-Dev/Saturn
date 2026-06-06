eacaf1474732da88b71d11634c01ad7e549ffb3f
Luis Saldanha <168227026+LuisSaldanha-Dev@users.noreply.github.com>
2026-06-06 19:41:32 -0300

    Adicionando[Componentes (GenericHtml) &  Pages (NotFound,AboutPomodoro) ]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Home } from './pages/Home';
-
 import './styles/theme.css';
 import './styles/global.css';
+
+import { Home } from './pages/Home';
+
+import { AboutPodomodoro } from './pages/AboutPomodoro';
+import { NotFound } from './pages/NotFound';
 
 export function App() {
   return <Home />;
