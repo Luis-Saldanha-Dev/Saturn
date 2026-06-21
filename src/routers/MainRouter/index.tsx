53bfdf866966bdd57c4767f0577df19515334dd3
Luis Saldanha <168227026+LuisSaldanha-Dev@users.noreply.github.com>
2026-06-21 13:42:57 -0300

    Adicionando dicionario, corrigindo formato de data, passando task ativa,abandonada,interrompida,completa

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router";
-import { Home } from "../../pages/Home";
-import { AboutPodomodoro } from "../../pages/AboutPomodoro";
-import { NotFound } from "../../pages/NotFound";
-import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { Home } from '../../pages/Home';
+import { History } from '../../pages/History';
+import { AboutPodomodoro } from '../../pages/AboutPomodoro';
+import { NotFound } from '../../pages/NotFound';
+import { useEffect } from 'react';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -14,15 +15,16 @@ function ScrollToTop() {
   return null;
 }
 
-export function MainRouter(){
-  return(
+export function MainRouter() {
+  return (
     <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro' element={<AboutPodomodoro />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-          <ScrollToTop />
-        </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/history' element={<History />} />
+        <Route path='/about-pomodoro' element={<AboutPodomodoro />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
   );
 }
