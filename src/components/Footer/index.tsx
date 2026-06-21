3b8a76c6b39bdec66e423a816fb524eb95498b76
Luis Saldanha <168227026+LuisSaldanha-Dev@users.noreply.github.com>
2026-06-21 10:42:44 -0300

    introduzindo React-Router na aplicacao

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,10 @@
+import { Link } from 'react-router';
 import styles from './styles.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=''>Descubra como funciona a técnica Pomodoro. 🍅</a>
-      <a href=''>
-        Saturn Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
-      </a>
+      <Link to='/about-pomodoro'>Descubra como funciona a técnica Pomodoro. 🍅</Link>
+      <Link to='/'>Saturn Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚</Link>
     </footer>
   );
 }
